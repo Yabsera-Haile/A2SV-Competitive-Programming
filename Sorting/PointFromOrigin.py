e298d8752c251e88c932c65cff832647e6c3667c
Yabsera Haile Yemanberhan <haileyabsera3@gmail.com>
2022-10-15 23:11:51 +0300

    question 11

---
+++ Sorting/PointFromOrigin.py
@@ -0,0 +1,7 @@
+import math
+
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points.sort(key=lambda point: point[0]**2 + point[1]**2)
+        return points[:k]
