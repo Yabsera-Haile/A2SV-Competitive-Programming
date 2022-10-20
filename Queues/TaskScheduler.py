b177ee606d3cb06b3cdee8778e15c18af57df2b4
Yabsera Haile Yemanberhan <haileyabsera3@gmail.com>
2022-10-20 18:19:13 +0300

    update

--- Queues/TaskScheduler.py
+++ Queues/TaskScheduler.py
@@ -1,15 +1,14 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        count = {}
-        maximum = 0
-        for i in tasks:
-            if i in count:
-                count[i] += 1
+        counts = {}
+        for task in tasks:
+            if task in counts:
+                counts[task] += 1
             else:
-                count[i] = 1
-            maximum = max(maximum, count[i])
-        output = (maximum-1)*(n+1)
-        for i in count.values():
-            if i == maximum:
-                output += 1
-        return max(output, len(tasks))
+                counts[task] = 1
+        maximum = max(counts.values())
+        result = (maximum-1)*(n+1)
+        for count in counts.values():
+            if count == maximum:
+                result += 1
+        return max(result, len(tasks))
