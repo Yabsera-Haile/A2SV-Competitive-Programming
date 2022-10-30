c84da4a2b87340d77cbab6bebfea87c6f0826f2b
Yabsera Haile Yemanberhan <haileyabsera3@gmail.com>
2022-10-30 21:17:26 +0300

    question 34

---
+++ Two Pointers/RemoveDuplicate.py
@@ -0,0 +1,14 @@
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = prev = ListNode(0)
+        dummy.next = head
+        while head and head.next:
+            if head.val == head.next.val:
+                while head and head.next and head.val == head.next.val:
+                    head = head.next
+                head = head.next
+                prev.next = head
+            else:
+                prev = prev.next
+                head = head.next
+        return dummy.next
