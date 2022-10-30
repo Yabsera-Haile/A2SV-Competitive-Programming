e376620260b594ec7ba49744c7e6ea9b2a155726
Yabsera Haile Yemanberhan <haileyabsera3@gmail.com>
2022-10-30 21:45:18 +0300

    question 40

---
+++ Two Pointers(Sliding Window)/MaximumSum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSumTwoNoOverlap(self, A: List[int], L: int, M: int) -> int:
+        def maxSum(L: int, M: int) -> int:
+            maxL = ans = 0
+            for i in range(L + M, len(prefixSum)):
+                maxL = max(maxL, prefixSum[i-M] - prefixSum[i-M-L])
+                ans = max(ans, maxL+prefixSum[i]-prefixSum[i-M])
+            return ans
+
+        prefixSum = [0] * (len(A) + 1)
+        for i, a in enumerate(A):
+            prefixSum[i + 1] = prefixSum[i] + a
+        return max(maxSum(L, M), maxSum(M, L))
+
+        return (result[0]+result[1])
